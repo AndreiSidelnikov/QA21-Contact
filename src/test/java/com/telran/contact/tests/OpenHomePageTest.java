a64db197487475d567eee3bd624a329b599229bd
Andrei <sidelnikovandrey@gmail.com>
2021-11-04 00:26:18 +0100

    Added testng.xml

--- src/test/java/com/telran/contact/tests/OpenHomePageTest.java
+++ src/test/java/com/telran/contact/tests/OpenHomePageTest.java
@@ -3,12 +3,12 @@ package com.telran.contact.tests;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class OpenHomePageTests extends TestBase{
+public class OpenHomePageTest extends TestBase{
 
     @Test
     public void homePageTest() throws InterruptedException {
         System.out.println("site opened");
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         System.out.println("HomeComponent:" + app.getHome().isElementPresent2(By.cssSelector("div:nth-child(2) > div > div")));
     }
 }
