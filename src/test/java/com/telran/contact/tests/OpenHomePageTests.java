ae47040564ac7b38e205098c7a04e0aa97ef9775
Andrei <sidelnikovandrey@gmail.com>
2021-11-04 00:01:55 +0100

    Added testng.xml

--- src/test/java/com/telran/contact/tests/OpenHomePageTests.java
+++ src/test/java/com/telran/contact/tests/OpenHomePageTests.java
@@ -3,7 +3,7 @@ package com.telran.contact.tests;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class OpenHomePageTests extends TestBase {
+public class OpenHomePageTests extends TestBase{
 
     @Test
     public void homePageTest() throws InterruptedException {
